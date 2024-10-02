a703d63411d98064d8e46b5c702651fef7f53420
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-10-02 20:50:00 +0530

    added socials and ui fixes installed bootstrap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import MenuButton from "./menu/menuButton";
 import Menu from "./menu/menu";
+import Socials from "../../socials/socials";
 
 const Home: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -13,29 +14,28 @@ const Home: React.FC = () => {
   return (
     <>
       {/* --------------------------PAGE 1 -------------------------------*/}
-
-      <div className=" h-[90vh]  bg-[#F1FAEE]  ">
-        <div className="flex">
-          <div className="grow flex-1"></div>
-          <div
-            className={`w-[40%] flex py-2 transition-all duration-300 ${
-              menuOpen ? "shadow-lg" : "border-transparent text-[#141414]"
-            } gap-10 items-center border justify-end pr-5 pl-2`}
-          >
-            <Menu isOpen={menuOpen} />
-            <MenuButton toggleMenu={toggleMenu} menuOpen={menuOpen} />
-          </div>
+      <div className="fixed shadow-lg z-50 flex flex-col gap-5 top-44  left-0 border-2 px-4 py-4 ">
+        <Socials />
+      </div>
+      <div className="relative h-[85vh] bg-[#F1FAEE]  ">
+        <div
+          className={`w-[40%] fixed right-0 top-0 flex py-2 transition-all duration-300 ${
+            menuOpen ? "shadow-lg" : "border-transparent text-[#141414]"
+          } gap-10 items-center border justify-end pr-5 pl-2`}
+        >
+          <Menu isOpen={menuOpen} />
+          <MenuButton toggleMenu={toggleMenu} menuOpen={menuOpen} />
         </div>
         <div className="flex">
-          <div className="mt-14">
-            <div className=" ml-20 text-7xl font-bold text-[#37D299]">
+          <div className="mt-14 ml-24">
+            <div className="  text-7xl font-bold text-[#37D299]">
               {" "}
               Nischal Mantri
             </div>
-            <div className="ml-20 mt-4  text-6xl font-bold text-[#111827]">
+            <div className=" mt-4  text-6xl font-bold text-[#111827]">
               Full Stack Developer
             </div>
-            <p className="ml-20 mt-4 font-xl text-[#111827]">
+            <p className=" mt-10 text-pretty text-sm font-semibold text-[#111827]">
               I’m passionate about clean, functional design, believing in the
               power of minimalism—where every line of code has a purpose and
               every pixel tells a story. Whether I'm building robust back-end
@@ -43,7 +43,7 @@ const Home: React.FC = () => {
               always on achieving a perfect balance between simplicity and
               functionality.
             </p>
-            <p className="ml-20  mt-4 font-xl text-[#111827]">
+            <p className=" mt-4 text-pretty text-sm font-semibold text-[#111827]">
               When I’m not immersed in coding, you’ll likely find me diving into
               the latest tech articles or exploring new frameworks. In my free
               time, I enjoy gaming and staying active at the gym, always
@@ -51,20 +51,21 @@ const Home: React.FC = () => {
               where I connect with fellow enthusiasts, share insights, and
               collaborate on exciting projects.
             </p>
-            <p className="ml-20 mt-4 font-xl text-[#111827]">
+            <p className=" mt-4 text-pretty text-sm font-semibold text-[#111827]">
               Whether it’s a challenging workout, an intense gaming session, or
               a stimulating tech conference, I’m constantly pushing my limits
               and seeking new ways to grow both personally and professionally.
             </p>
           </div>
-          <div className=" text-black w-[105%] flex justify-center items-center ">
+          <div className="  w-[105%] flex justify-center items-center ">
             {" "}
-            <div className="border-2 mt-[5rem]">
+            <div className="border-4 mt-[8rem]  overflow-hidden shadow-black-xl">
               <img
-                className="rounded-xl"
-                src="https://thispersondoesnotexist.com"
+                className=""
+                src="https://media.gq.com/photos/646baa821fa990bc7018e902/master/w_1920,c_limit/GQ0723_Gosling_01.jpg"
                 alt="My_Image"
                 width="450px"
+                height="200px"
               />
             </div>
           </div>
