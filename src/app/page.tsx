00f8e66271106e886c6fbe7cfb479e97fb02b7e1
Mantri Venkata Sai Nischal <sainischalmv@gmail.com>
2024-10-19 20:06:03 +0530

    phone view for the first page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ const Home: React.FC = () => {
     <>
       {/* -------------------------- PAGE 1 ------------------------------- */}
       <div
-        className="fixed shadow-lg z-50 flex flex-col gap-5 top-44 left-0 border-2 px-4 py-4
+        className="fixed shadow-lg z-50  hidden lg:flex flex-col gap-5 top-44 left-0 border-2 px-4 py-4
         mix-blend-difference "
         style={{
           background: "rgba(255, 255, 255, 0.05)",
@@ -79,23 +79,37 @@ const Home: React.FC = () => {
       >
         <Socials />
       </div>
-      <div className="relative w-full  h-[85vh] bg-[#F1FAEE]" id="page1">
-        <div className="flex justify-end">
+
+      <div
+        className="relative w-full h-[85vh] bg-[#F1FAEE] pt-10 lg:pt-0 "
+        id="page1"
+      >
+        <div className=" lg:hidden overflow-hidden shadow-black-xl flex justify-center items-center rounded-full">
+          <Image
+            className="rounded-full aspect-square"
+            src="https://media.gq.com/photos/646baa821fa990bc7018e902/master/w_1920,c_limit/GQ0723_Gosling_01.jpg"
+            alt="My_Image"
+            width={100}
+            height={100}
+          />
+        </div>
+
+        <div className="hidden lg:flex justify-end">
           <NavBar
             scrollToRef={scrollToRef}
             contactRef={contactRef}
             projectsRef={projectsRef}
           />
         </div>
-        <div className="flex">
-          <div className="mt-14 ml-24">
-            <div className="text-7xl font-bold text-[#37D299]">
-              Nischal Mantri
+        <div className="flex mx-5">
+          <div className=" mt-4  lg:mt-14 lg:ml-24">
+            <div className="text-4xl w-fit text-nowrap  items-center justify-center lg:text-7xl font-bold flex gap-4 text-[#37D299]">
+              <div className=""> Nischal Mantri</div>
             </div>
-            <div className="mt-4 text-6xl font-bold text-[#111827]">
+            <div className="  lg:mt-4 text-lg lg:text-6xl font-bold text-[#111827]">
               Full Stack Developer
             </div>
-            <p className="mt-10 text-pretty text-sm font-semibold text-[#111827]">
+            <p className="lg:mt-10 mt-4  text-pretty lg:text-sm text-xs  font-semibold text-[#111827]">
               I’m passionate about clean, functional design, believing in the
               power of minimalism—where every line of code has a purpose and
               every pixel tells a story. Whether I’m building robust back-end
@@ -103,7 +117,8 @@ const Home: React.FC = () => {
               always on achieving a perfect balance between simplicity and
               functionality.
             </p>
-            <p className="mt-4 text-pretty text-sm font-semibold text-[#111827]">
+
+            <p className="lg:block text-xs hidden mt-4 text-pretty lg:text-sm font-semibold text-[#111827]">
               When I’m not immersed in coding, you’ll likely find me diving into
               the latest tech articles or exploring new frameworks. In my free
               time, I enjoy gaming and staying active at the gym, always
@@ -111,13 +126,13 @@ const Home: React.FC = () => {
               where I connect with fellow enthusiasts, share insights, and
               collaborate on exciting projects.
             </p>
-            <p className="mt-4 text-pretty text-sm font-semibold text-[#111827]">
+            <p className=" mt-4 text-pretty text-xs lg:text-sm font-semibold text-[#111827]">
               Whether it’s a challenging workout, an intense gaming session, or
               a stimulating tech conference, I’m constantly pushing my limits
               and seeking new ways to grow both personally and professionally.
             </p>
           </div>
-          <div className="w-[105%] flex justify-center items-center">
+          <div className=" hidden lg:flex lg:w-[105%]   lg:justify-center lg:items-center">
             <div className="border-4 mt-[4rem] overflow-hidden shadow-black-xl">
               <Image
                 src="https://media.gq.com/photos/646baa821fa990bc7018e902/master/w_1920,c_limit/GQ0723_Gosling_01.jpg"
@@ -127,6 +142,20 @@ const Home: React.FC = () => {
               />
             </div>
           </div>
+        </div>
+        <div
+          className=" lg:hidden flex  space-x-10 mt-10  justify-center items-center px-4 py-4
+        mix-blend-difference "
+          // style={{
+          //   background: "rgba(255, 255, 255, 0.05)",
+          //   boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.37)",
+          //   backdropFilter: "blur(3.5px)",
+          //   WebkitBackdropFilter: "blur(3.5px)",
+          //   borderRadius: "10px",
+          //   border: "1px solid rgba(255, 255, 255, 0.18)",
+          // }}
+        >
+          <Socials />
         </div>
       </div>
       <div className="w-full h-[10vh]"></div>
